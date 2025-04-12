9aefdfcb7c097ed221c95f10f131fcbed9c2abe5
duanegero <duanegero11@gmail.com>
2025-04-12 11:11:08 -0300

    adding home page display and styles, tested

--- src/Components/Home Page/allProfile-cards.jsx
+++ src/Components/Home Page/allProfile-cards.jsx
@@ -1,6 +1,7 @@
 //imports from React
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { TbBrandMatrix } from "react-icons/tb";
 
 import fetchAllUsers from "./Helpers/fetchAllUsers";
 
@@ -34,41 +35,68 @@ export default function AllProfileCards() {
 
         return (
           <div key={id}>
-            <div>
+            <header className="flex justify-between items-center border-b-2 border-b-gray-400 bg-gray-200">
+              <p className="flex items-center pl-4 font-rubikone text-3xl tracking-wider text-gray-400 pt-4 pb-4">
+                <TbBrandMatrix />
+                Matrix Profiles
+              </p>
+            </header>
+            <div className="flex items-center border-b-2 bg-gray-100">
               {profile_pic_url && (
                 <img
                   src={profile_pic_url}
                   alt={`${firstname} ${lastname}'s Profile`}
+                  className="w-64 h-64 mt-6 mb-6 ml-10  border-2 border-gray-200 rounded-full object-cover max-w-full max-h-full"
                 />
               )}
               <h2 className="ml-10 font-rubikone text-4xl text-gray-600">
                 {firstname} {lastname}
               </h2>
             </div>
-            <div className="grid grid-cols-2 gap-y-6 gap-x-10">
-              <div>
-                <h3>BIO:</h3>
-                <p>{bio}</p>
-              </div>
-              <div>
-                <h3>Completion Year:</h3>
-                <p>{completion_year}</p>
-              </div>
-              <div>
-                <h3>Linkedin:</h3>
-                <a
-                  href={linkedin_url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {linkedin_url}
-                </a>
-              </div>
-              <div>
-                <h3>GitHub:</h3>
-                <a href={github_url} target="_blank" rel="noopener noreferrer">
-                  {github_url}
-                </a>
+            <div className="bg-gray-200">
+              <div className="justify-center items-center grid grid-cols-2 gap-y-8 gap-x-12 p-4 pt-10 pb-16">
+                <div className="flex flex-col">
+                  <h3 className="font-rubikone text-3xl tracking-wider text-gray-400">
+                    BIO:
+                  </h3>
+                  <p className="font-rubik tracking-wide text-lg text-gray-500">
+                    {bio}
+                  </p>
+                </div>
+                <div className="flex flex-col">
+                  <h3 className="font-rubikone text-3xl tracking-wider text-gray-400">
+                    Completion Year:
+                  </h3>
+                  <p className="font-rubik tracking-wide text-lg text-gray-500">
+                    {completion_year}
+                  </p>
+                </div>
+                <div className="flex flex-col">
+                  <h3 className="font-rubikone text-3xl tracking-wider text-gray-400">
+                    Linkedin:
+                  </h3>
+                  <a
+                    href={linkedin_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-500 text-lg underline hover:text-blue-700"
+                  >
+                    {linkedin_url}
+                  </a>
+                </div>
+                <div className="flex flex-col">
+                  <h3 className="font-rubikone text-3xl tracking-wider text-gray-400">
+                    GitHub:
+                  </h3>
+                  <a
+                    href={github_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-500 text-lg underline hover:text-blue-700"
+                  >
+                    {github_url}
+                  </a>
+                </div>
               </div>
             </div>
           </div>
