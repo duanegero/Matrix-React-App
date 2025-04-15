2f56c30e38c733829a8433eef05dc35de7578673
duanegero <duanegero11@gmail.com>
2025-04-15 13:20:26 -0300

    adding home button to login page

---
+++ src/Components/Home Page/Helpers/navigateHome.jsx
@@ -0,0 +1,8 @@
+//defining a function with passed in variable
+const naviageHome = (navigate) => {
+  //path to navigate to
+  navigate("/");
+};
+
+//export function to use else where
+export default naviageHome;
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import LoginForm from "../Components/Login Page/login-form";
+import naviageHome from "../Components/Home Page/Helpers/navigateHome";
 
 export default function Login() {
   useEffect(() => {
@@ -12,8 +14,16 @@ export default function Login() {
     };
   });
 
+  const navigate = useNavigate();
+
   return (
     <>
+      <button
+        className="absolute top-5 right-5 flex items-center text-2xl p-3 font-rubikone text-gray-400 hover:text-blue-400 cursor-pointer"
+        onClick={() => naviageHome(navigate)}
+      >
+        Home
+      </button>
       <LoginForm />
     </>
   );
