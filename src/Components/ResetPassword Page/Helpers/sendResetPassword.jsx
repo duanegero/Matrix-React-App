9f4946132aef3932b7af1cc3281cf111722c52fc
duanegero <duanegero11@gmail.com>
2025-04-15 12:34:13 -0300

    still trying

--- src/Components/ResetPassword Page/Helpers/sendResetPassword.jsx
+++ src/Components/ResetPassword Page/Helpers/sendResetPassword.jsx
@@ -1,43 +1,28 @@
-//importing supabase
+// importing supabase
 import { supabase } from "../../../supabaseClient";
 
-//defining function with passed in variables
+// defining function
 const sendResetPassword = async (
   password,
-  token,
   setPassword,
   setConfirmPassword,
   setMessage
 ) => {
   try {
-    const { error: sessionError } = await supabase.auth.setSession({
-      access_token: token,
-      refresh_token: "",
-    });
-
-    if (sessionError) {
-      setMessage("Invalid or expired link.");
-      return;
-    }
-
-    //query to supabase confirming user is auth from reset link
-    const { error } = await supabase.auth.api.resetPasswordForEmail({
+    const { data, error } = await supabase.auth.updateUser({
       password,
     });
 
     if (error) {
-      setMessage("Error resetting password:" + error.message);
-      return;
+      setMessage("Error resetting password: " + error.message);
     } else {
-      //else clear inputs, set success message
       setPassword("");
       setConfirmPassword("");
-      setMessage("Password updated, navigate to login.");
+      setMessage("Password updated successfully. Please log in again.");
     }
   } catch (error) {
-    setMessage("Unexpected error:" + error.message);
+    setMessage("Unexpected error: " + error.message);
   }
 };
 
-//export function to use else where
 export default sendResetPassword;
